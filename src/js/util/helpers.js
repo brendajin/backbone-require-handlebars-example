b40056f1cdadf97b6dcf7239254ac37e36e21c37
Brenda Jin <brendajin@gmail.com>
2013-12-11 16:39:13 -0800

    finishing touches on that star rating and star count

--- src/js/util/helpers.js
+++ src/js/util/helpers.js
@@ -3,7 +3,28 @@ define([
 ], function(Handlebars){
 
 	Handlebars.registerHelper("rating", function(rating){
+		var starRating = '';
+		var darkStars = rating.starRating.charAt(0);
+
 		console.log(rating)
-		return '<span>something to hold a place here</span>'
+
+		if(rating.starRating.charAt(0)) {
+			for (var i = rating.starRating.charAt(0); i >0; i--) {
+				starRating += '<span class="icon-star3"></span>';
+			}
+		}
+
+		if(rating.starRating.charAt(1) == '.') {
+			starRating += '<span class="icon-star2"></span>';
+			darkStars = parseInt(darkStars) + 1;
+		}
+
+		for(var i = 5-darkStars; i > 0; i--) {
+			starRating += '<span class="icon-star"></span>';
+		}
+
+
+
+		return starRating + ' ('+rating.count+')';
 	})	
 })
